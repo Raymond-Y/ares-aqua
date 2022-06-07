d92396f0ae8b4e4f1eedbdb6cc23f497277a4f77
Raymond-Y <s4589619@student.uq.edu.au>
2022-06-08 08:20:31 +1000

    Update beacon locations

--- code/python_software/newProject.py
+++ code/python_software/newProject.py
@@ -53,12 +53,12 @@ class BaseProcessing:
         self.rssi_data_samples_nodeK = np.ones(ARRAY_SIZE)
         self.rssi_data_samples_nodeL = np.ones(ARRAY_SIZE)
         self.size_received_beacons = 0
-        self.x0 = 3
-        self.y0 = 2
-        self.mobileNode1x = 3
-        self.mobileNode1y = 4
-        self.mobileNode2x = 1
-        self.mobileNode2y = 2
+        # self.x0 = 3
+        # self.y0 = 2
+        self.mobileNode1x = 0
+        self.mobileNode1y = 0
+        self.mobileNode2x = 0
+        self.mobileNode2y = 0
         self.receivedXPositions = np.ones(5)
         self.receivedYPositions = np.ones(5)
         self.receivedRSSI = np.ones(5)
@@ -71,9 +71,12 @@ class BaseProcessing:
         self.mobilenode1, = self.ax.plot([],[],'go', markersize='12')
         self.mobilenode2, = self.ax.plot([],[],'ro', markersize='12')
         
-        self.ax.axis([0, 41, 0, 21])
+        #self.ax.axis([0, 41, 0, 21])
+        self.ax.axis([0, 32, 0, 21])
         self.setup_beacons()
+        self.ax.plot(self.beacon_xvals,self.beacon_yvals,'bo', markersize='4')
         self.writeFile = open('resultsStatic.csv','w')
+        self.tempcount =0
     def setup_beacons(self):
         # inputfile = open('BeaconData.csv','r')
         # for row in inputfile:
@@ -95,21 +98,27 @@ class BaseProcessing:
     def update(self,extra):
         self.lock_rssi_coords.acquire()
         #self.graph.set_data(self.x0,self.y0)
+        #  BaseProcessing.mobileNode1x = random.randint(5,15)
+        # BaseProcessing.mobileNode1y = random.randint(8,10)
+        # print("Coords:%f,%f" % (BaseProcessing.x0, BaseProcessing.y0))
+        # print("MobileNode1:%f,%f" % (BaseProcessing.mobileNode1x, BaseProcessing.mobileNode1y))
         self.mobilenode1.set_data(self.mobileNode1x, self.mobileNode1y)
         self.mobilenode2.set_data(self.mobileNode2x, self.mobileNode2y)
-        self.fig.suptitle("At X:%f, Y:%f, MobileNode:(%f,%f)" %(self.x0, self.y0, self.mobileNode1x, self.mobileNode1y))
+        self.fig.suptitle(" MobileNode1:(%f,%f), MobileNode2:(%f,%f)" %(self.mobileNode1x, self.mobileNode1y, self.mobileNode2x, self.mobileNode2y))
 
         self.lock_rssi_coords.release()
         
 
     def animate(self):
-        self.anim = animation.FuncAnimation(self.fig, self.update,interval=200, blit=False)
+        self.anim = animation.FuncAnimation(self.fig, self.update,interval=400, blit=False)
 
 
     def calculate_positions(self):
         self.lock_rssi_coords.acquire()
-        self.lock_data.acquire()
+        #self.lock_data.acquire()
         print("ENTER CALCULATE POSITIONS")
+        self.tempcount+= 1
+        print("Self count%i", self.tempcount)
         if (self.size_received_beacons == 3):
             r1 = self.receivedRSSI[0]
             r2 = self.receivedRSSI[1]
@@ -136,6 +145,15 @@ class BaseProcessing:
             A = np.vstack([Ax_array, Ay_array]).T
        
             x0, y0 = np.linalg.lstsq(A,B_array,rcond=None)[0]
+            
+            #ADD KNN
+
+            if (x0 < 2):
+                x0 = 2
+            if (y0 < 4.1):
+                y0 = 4.1
+
+
             if(self.receivedMobileNodeNumber == 1):
                 self.mobileNode1x = x0
                 self.mobileNode1y = y0
@@ -171,6 +189,14 @@ class BaseProcessing:
             A = np.vstack([Ax_array, Ay_array]).T
        
             x0, y0 = np.linalg.lstsq(A,B_array,rcond=None)[0]
+            #ADD KNN
+
+            if (x0 < 2):
+                x0 = 2
+            if (y0 < 4.1):
+                y0 = 4.1
+
+
             if(self.receivedMobileNodeNumber == 1):
                 self.mobileNode1x = x0
                 self.mobileNode1y = y0
@@ -204,12 +230,12 @@ class BaseProcessing:
             Ax1 = (2*(x5-x1))
             Ax2 = (2*(x5-x2))
             Ax3 = (2*(x5-x3))
-            Ax3 = (2*(x5-x4))
+            Ax4 = (2*(x5-x4))
             
             Ay1 = (2*(y5-y1))
             Ay2 = (2*(y5-y2))
             Ay3 = (2*(y5-y3))
-            Ay3 = (2*(y5-y4))
+            Ay4 = (2*(y5-y4))
 
             Ax_array = np.array([Ax1, Ax2, Ax3, Ax4])
             Ay_array = np.array([Ay1, Ay2, Ay3, Ay4])
@@ -217,6 +243,14 @@ class BaseProcessing:
             A = np.vstack([Ax_array, Ay_array]).T
        
             x0, y0 = np.linalg.lstsq(A,B_array,rcond=None)[0]
+            #ADD KNN
+
+            if (x0 < 2):
+                x0 = 2
+            if (y0 < 4.1):
+                y0 = 4.1
+            
+
             if(self.receivedMobileNodeNumber == 1):
                 self.mobileNode1x = x0
                 self.mobileNode1y = y0
@@ -224,138 +258,11 @@ class BaseProcessing:
                 self.mobileNode2x = x0
                 self.mobileNode2y = y0
         self.lock_rssi_coords.release()
-        self.lock_data.release()
+        #self.lock_data.release()
 
 
     def test(self):
         print(self.x0 * self.y0)
-def perform_least_square(BaseProcessing):
-    while True:
-        BaseProcessing.lock_data.acquire()
-        r1 = np.mean(BaseProcessing.rssi_data_samples_nodeA)
-        r2 = np.mean(BaseProcessing.rssi_data_samples_nodeB)
-        r3 = np.mean(BaseProcessing.rssi_data_samples_nodeC)
-        r4 = np.mean(BaseProcessing.rssi_data_samples_nodeD)
-        r5 = np.mean(BaseProcessing.rssi_data_samples_nodeE)
-        r6 = np.mean(BaseProcessing.rssi_data_samples_nodeF)
-        r7 = np.mean(BaseProcessing.rssi_data_samples_nodeG)
-        r8 = np.mean(BaseProcessing.rssi_data_samples_nodeH)
-        r9 = np.mean(BaseProcessing.rssi_data_samples_nodeI)
-        r10 = np.mean(BaseProcessing.rssi_data_samples_nodeJ)
-        r11= np.mean(BaseProcessing.rssi_data_samples_nodeK)
-        r12= np.mean(BaseProcessing.rssi_data_samples_nodeL)
-        x1 = BaseProcessing.beacon_xvals[0]
-        x2 = BaseProcessing.beacon_xvals[1]
-        x3 = BaseProcessing.beacon_xvals[2]
-        x4 = BaseProcessing.beacon_xvals[3]
-        x5 = BaseProcessing.beacon_xvals[4]
-        x6 = BaseProcessing.beacon_xvals[5]
-        x7 = BaseProcessing.beacon_xvals[6]
-        x8 = BaseProcessing.beacon_xvals[7]
-        x9 = BaseProcessing.beacon_xvals[8]
-        x10 = BaseProcessing.beacon_xvals[9]
-        x11 = BaseProcessing.beacon_xvals[10]
-        x12 = BaseProcessing.beacon_xvals[11]
-        y1 = BaseProcessing.beacon_yvals[0]
-        y2 = BaseProcessing.beacon_yvals[1]
-        y3 = BaseProcessing.beacon_yvals[2]
-        y4 = BaseProcessing.beacon_yvals[3]
-        y5 = BaseProcessing.beacon_yvals[4]
-        y6 = BaseProcessing.beacon_yvals[5]
-        y7 = BaseProcessing.beacon_yvals[6]
-        y8 = BaseProcessing.beacon_yvals[7]
-        y9 = BaseProcessing.beacon_yvals[8]
-        y10 = BaseProcessing.beacon_yvals[9]
-        y11 = BaseProcessing.beacon_yvals[10]
-        y12 = BaseProcessing.beacon_yvals[11]
-        # print("A")
-        # print(BaseProcessing.rssi_data_samples_nodeA)
-        # print("B")
-        # print(BaseProcessing.rssi_data_samples_nodeB)
-        # print("C")
-        # print(BaseProcessing.rssi_data_samples_nodeC)
-        # print("D")
-        # print(BaseProcessing.rssi_data_samples_nodeD)
-        # print("E")
-        # print(BaseProcessing.rssi_data_samples_nodeE)
-        # print("F")
-        # print(BaseProcessing.rssi_data_samples_nodeF)
-        # print("G")
-        # print(BaseProcessing.rssi_data_samples_nodeG)
-        # print("H")
-        # print(BaseProcessing.rssi_data_samples_nodeH)
-        # print("I")
-        # print(BaseProcessing.rssi_data_samples_nodeI)
-        # print("J")
-        # print(BaseProcessing.rssi_data_samples_nodeJ)
-        # print("K")
-        # print(BaseProcessing.rssi_data_samples_nodeK)
-        # print("L")
-        # print(BaseProcessing.rssi_data_samples_nodeL)
-
-        b1 = (r1**2) -(r12**2) - (x1**2) -(y1**2) +(x12**2) + (y12**2)
-        b2 = (r2**2) -(r12**2) - (x2**2) -(y2**2) +(x12**2) + (y12**2)
-        b3 = (r3**2) -(r12**2) - (x3**2) -(y3**2) +(x12**2) + (y12**2)
-        b4 = (r4**2) -(r12**2) - (x4**2) -(y4**2) +(x12**2) + (y12**2)
-        b5 = (r5**2) -(r12**2) - (x5**2) -(y5**2) +(x12**2) + (y12**2)
-        b6 = (r6**2) -(r12**2) - (x6**2) -(y6**2) +(x12**2) + (y12**2)
-        b7 = (r7**2) -(r12**2) - (x7**2) -(y7**2) +(x12**2) + (y12**2)
-        b8 = (r8**2) -(r12**2) - (x8**2) -(y8**2) +(x12**2) + (y12**2)
-        b9 = (r9**2) -(r12**2) - (x9**2) -(y9**2) +(x12**2) + (y12**2)
-        b10 = (r10**2) -(r12**2) - (x10**2) -(y10**2) +(x12**2) + (y12**2)
-        b11 = (r11**2) -(r12**2) - (x11**2) -(y11**2) +(x12**2) + (y12**2)
-        Ax1 = (2*(x12-x1))
-        Ax2 = (2*(x12-x2))
-        Ax3 = (2*(x12-x3))
-        Ax4 = (2*(x12-x4))
-        Ax5 = (2*(x12-x5))
-        Ax6 = (2*(x12-x6))
-        Ax7 = (2*(x12-x7))
-        Ax8 = (2*(x12-x8))
-        Ax9 = (2*(x12-x9))
-        Ax10 = (2*(x12-x10))
-        Ax11 = (2*(x12-x11))
-        Ay1 = (2*(y12-y1))
-        Ay2 = (2*(y12-y2))
-        Ay3 = (2*(y12-y3))
-        Ay4 = (2*(y12-y4))
-        Ay5 = (2*(y12-y5))
-        Ay6 = (2*(y12-y6))
-        Ay7 = (2*(y12-y7))
-        Ay8 = (2*(y12-y8))
-        Ay9 = (2*(y12-y9))
-        Ay10 = (2*(y12-y10))
-        Ay11 = (2*(y12-y11))
-        Ax_array = np.array([Ax1, Ax2, Ax3, Ax4, Ax5, Ax6, Ax7, Ax8, Ax9, Ax10, Ax11])
-        Ay_array = np.array([Ay1, Ay2, Ay3, Ay4, Ay5, Ay6, Ay7, Ay8, Ay9, Ay10, Ay11])
-        B_array = np.array([b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11])
-        A = np.vstack([Ax_array, Ay_array]).T
-        BaseProcessing.lock_rssi_coords.acquire()
-        x0, y0 = np.linalg.lstsq(A,B_array,rcond=None)[0]
-        if(x0 > 4):
-            x0 = 4
-        if(x0 < 0):
-            x0 = 0
-        if(y0 > 4):
-            y0 = 4
-        if(y0 < 0):
-            y0 = 0
-        #FIXME 
-        x0 = random.randint(15,25)
-        y0 = random.randint(8,10)
-
-        BaseProcessing.x0 = x0
-        BaseProcessing.y0 = y0
-
-        BaseProcessing.mobileNode1x = random.randint(5,15)
-        BaseProcessing.mobileNode1y = random.randint(8,10)
-        print("Coords:%f,%f" % (BaseProcessing.x0, BaseProcessing.y0))
-        print("MobileNode1:%f,%f" % (BaseProcessing.mobileNode1x, BaseProcessing.mobileNode1y))
-        
-        BaseProcessing.lock_data.release()
-        BaseProcessing.lock_rssi_coords.release()
-        print("inside Least squartes")
-        time.sleep(1)
 
 
 
@@ -379,22 +286,26 @@ def update_data(BaseProcessing):
                 pass
         else:
             try:
-
+                print("trying to read")
                 output = BaseProcessing.serial_port.readline()
                 #print(output)
-                output_converted = ((str(output, 'utf-8'))[:-2])
+                output_converted = ((str(output, 'utf-8'))[:-1])
+                #FIXME REMOVE WHEN NOT TESTING
+                output_converted = '{"Mobile2, 2:-75, 4:-69, 5:-67, 10:-73,12:-59,}'
+                #output_converted = '{"Mobile2, 2:-75, 4:-69, 5:-67, 10:-73,}'
+                #output_converted = '{"Mobile2, 2:-75, 4:-69, 5:-60}'
                 if len(output_converted) > 1:
                     print(output_converted)
                     
                     #output_converted = '{"Node": 52, "rssi":93}'
                     #output_converted = '{"Node": 52, "rssi":93, "ultra":53}'
-
+                    #output_converted = '{"Mobile1, 2:-75, 4:-69, 5:-67, 10:-73,12:-59,'
                     #format = 
-                    outputs = output_converted[1:-1].replace(" ", "").split(",")
+                    outputs = output_converted[1:-2].replace(" ", "").split(",")
                     print(outputs)
                     print(len(outputs))
                     # 3 rssi readings
-                    BaseProcessing.lock_data.acquire()
+                    #BaseProcessing.lock_data.acquire()
                     if(len(outputs) == 4):
                         print("ACUUIRED 3 data")
                         mobile_node_number = int((outputs[0])[-1:])
@@ -421,10 +332,11 @@ def update_data(BaseProcessing):
                         receivedRSSI_actual2 = 10**((-53-float(receivedRSSI2))/ (10*2))
                         receivedRSSI_actual3 = 10**((-53-float(receivedRSSI3))/ (10*2))
 
-                        BaseProcessing.lock_data.release()
+                        #BaseProcessing.lock_data.release()
                         BaseProcessing.receivedRSSI[0] = receivedRSSI_actual1
                         BaseProcessing.receivedRSSI[1] = receivedRSSI_actual2
                         BaseProcessing.receivedRSSI[2] = receivedRSSI_actual3
+                        print("END ACUUIRED 3 data")
                         BaseProcessing.calculate_positions()
                         
                     if(len(outputs) == 5):
@@ -465,7 +377,7 @@ def update_data(BaseProcessing):
                         BaseProcessing.receivedRSSI[1] = receivedRSSI_actual2
                         BaseProcessing.receivedRSSI[2] = receivedRSSI_actual3
                         BaseProcessing.receivedRSSI[3] = receivedRSSI_actual4
-                        BaseProcessing.lock_data.release()
+                        #BaseProcessing.lock_data.release()
                         BaseProcessing.calculate_positions()
                         print("END ACUUIRED 4 data")
 
@@ -518,7 +430,7 @@ def update_data(BaseProcessing):
 
                         print("END ACUUIRED 5 data")
 
-                        BaseProcessing.lock_data.release()
+                        #BaseProcessing.lock_data.release()
                         BaseProcessing.calculate_positions()
                     
                             
@@ -588,12 +500,6 @@ def main():
     reading_thr = threading.Thread(target = update_data, args=(baseProgram,))
     reading_thr.setDaemon(True)
     reading_thr.start()
-    lsq_thr = threading.Thread(target = perform_least_square, args=(baseProgram,))
-    lsq_thr.setDaemon(True)
-    #lsq_thr.start()
-    #updategraph_thr = threading.Thread(target = updateGraph, args=(baseProgram,))
-    #updategraph_thr.setDaemon(True)
-    #updategraph_thr.start()
     baseProgram.animate()
     plt.show()
 
